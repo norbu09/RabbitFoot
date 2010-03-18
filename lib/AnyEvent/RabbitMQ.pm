9a45a9cae5cf05275e198e8ed66176f703189af9
Lenz Gschwendtner <lenz@springtimesoft.com>
2010-03-18 14:41:03 +1300

    changed POD to reflect changes to load_xml_spec

--- lib/AnyEvent/RabbitMQ.pm
+++ lib/AnyEvent/RabbitMQ.pm
@@ -469,9 +469,7 @@ AnyEvent::RabbitMQ - An asynchronous and multi channel Perl AMQP client.
 
   my $cv = AnyEvent->condvar;
 
-  my $ar = AnyEvent::RabbitMQ->new->load_xml_spec(
-      '/path/to/amqp0-8.xml',
-  )->connect(
+  my $ar = AnyEvent::RabbitMQ->new->load_xml_spec->connect(
       host       => 'localhosti',
       port       => 5672,
       user       => 'guest',
